a82d41f8ccfd62708af43666d291465f8b448b76
MarcDagher <marcdagher47@gmail.com>
2023-11-09 22:02:44 +0200

    Done filtering - note: error when editing then filtering. check edit

--- javascript/script.js
+++ javascript/script.js
@@ -3,11 +3,14 @@ let task_name = document.getElementById("task-name")
 let add_task_btn = document.getElementsByClassName("add-task")[0]
 let clear_btn = document.getElementsByClassName("clear")[0]
 let u_list = document.getElementsByTagName("ul")[0]
+// let task_storage = {}
 
 // Event: clear written input text
 clear_btn.addEventListener("click", function () {
   task_name.value = ""
 })
+
+// let counter = 0
 
 // ON-CLICK function to add INPUT to the list 
 add_task_btn.addEventListener("click", add_task_to_list)
@@ -22,7 +25,6 @@ function add_task_to_list () {
     // create and display list item
     let new_li = document.createElement("li")
     new_li.innerHTML = task_name.value
-    new_li.classList.add("Li") 
     u_list.appendChild(new_li) 
 
     // create and display delete button
@@ -37,14 +39,19 @@ function add_task_to_list () {
     edit_btn.setAttribute("class", "edit")
     new_li.appendChild(edit_btn)
 
+    // counter ++
+    // new_li.setAttribute("id", `${counter}`)
+    // task_storage. new_li
+
     // Event: toggle green on li item 
     new_li.addEventListener("click", function () {
       new_li.classList.toggle("green")
+      // console.log(u_list)
     })
     
     // Event: delete list item on delete button 
     delete_btn.addEventListener("click", function() {
-      delete_btn.parentElement.style.display="none"
+      delete_btn.parentElement.remove()
     })
 
     // Event: edit text on edit button
@@ -54,6 +61,44 @@ function add_task_to_list () {
       task_name.value = edit_btn.parentElement.textContent
       edit_btn.parentElement.style.display = "none"
     } )
+
+    // Event: filter according to select
+    let filter_input = document.getElementById("filter")
+    filter_input.addEventListener("click", function (e) {
+      filter_value = e.target.value // all task, completed, active
+
+      // console.log(previous_list_of_li)
+      // console.log(previous_list_of_li.children)
+
+      
+      for (i=0 ; i<u_list.children.length; i++){
+        
+        let previous_list_of_li = u_list.children[i].parentElement 
+        console.log(previous_list_of_li.children[i])
+        if (filter_value == "active") { // hide class="green"
+          if (previous_list_of_li.children[i].classList == 'green'){
+            previous_list_of_li.children[i].style.display = "none"  
+          } else if (previous_list_of_li.children[i].classList != 'green') {
+            previous_list_of_li.children[i].style.display = "inline-block"
+           }
+        }
+          
+        if (filter_value == "completed"){ // hide none class="green"
+            if (previous_list_of_li.children[i].classList != 'green'){
+              previous_list_of_li.children[i].style.display = "none"
+            } else if (previous_list_of_li.children[i].classList == 'green') {
+              previous_list_of_li.children[i].style.display = "inline-block"
+             } 
+          }
+        if (filter_value == "all tasks"){
+          previous_list_of_li.children[i].style.display ="inline-block"
+        }
+        
+      }
+
+      // console.log(u_list.children)
+      // console.log(filter_value)
+    })
     task_name.value = ""
   }
 }
